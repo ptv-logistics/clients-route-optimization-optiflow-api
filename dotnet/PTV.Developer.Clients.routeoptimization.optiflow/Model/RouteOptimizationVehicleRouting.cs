48282a894d691c520140b7c8a42e150a821dbdd1
MachineUserPTV <github@ptvgroup.com>
2024-11-04 13:12:18 +0000

    Automatic synchronisation from developer.myptv.com

--- dotnet/PTV.Developer.Clients.routeoptimization.optiflow/Model/RouteOptimizationVehicleRouting.cs
+++ dotnet/PTV.Developer.Clients.routeoptimization.optiflow/Model/RouteOptimizationVehicleRouting.cs
@@ -31,6 +31,12 @@ namespace PTV.Developer.Clients.routeoptimization.optiflow.Model
     [DataContract(Name = "VehicleRouting")]
     public partial class RouteOptimizationVehicleRouting : IValidatableObject
     {
+
+        /// <summary>
+        /// Gets or Sets Violations
+        /// </summary>
+        [DataMember(Name = "violations", EmitDefaultValue = true)]
+        public RouteOptimizationRoutingViolationStrategy? Violations { get; set; }
         /// <summary>
         /// Initializes a new instance of the <see cref="RouteOptimizationVehicleRouting" /> class.
         /// </summary>
@@ -41,8 +47,8 @@ namespace PTV.Developer.Clients.routeoptimization.optiflow.Model
         /// </summary>
         /// <param name="profile">A profile defines a vehicle by a set of attributes, matching typical transport situations. It must be the name of one of the predefined profiles *EUR_PEDESTRIAN*, *EUR_BICYCLE*, *EUR_CAR*, *EUR_VAN*, *EUR_TRUCK_7_49T*, *EUR_TRUCK_11_99T*, *EUR_TRUCK_40T* or *EUR_TRAILER_TRUCK*. At most ten profiles may be used within a single optimization.   Please note that the upper bound on number of different routing profiles is a technical limit. Check your individual price plan or contract to see which limits apply.  (required).</param>
         /// <param name="speedFactor">An additional factor to apply to the speed of the vehicle. When lower than one, the driving durations of the vehicle will increase, when greater than one, the driving durations of the vehicle will decrease.  (default to 1D).</param>
-        /// <param name="allowViolations">When enabled, a vehicle is permitted to travel on segments prohibited by its profile.  These prohibited segments will only be used when no valid alternatives are available.  (default to true).</param>
-        public RouteOptimizationVehicleRouting(string profile = default(string), double? speedFactor = 1D, bool? allowViolations = true)
+        /// <param name="violations">violations.</param>
+        public RouteOptimizationVehicleRouting(string profile = default(string), double? speedFactor = 1D, RouteOptimizationRoutingViolationStrategy? violations = default(RouteOptimizationRoutingViolationStrategy?))
         {
             // to ensure "profile" is required (not null)
             if (profile == null)
@@ -52,8 +58,7 @@ namespace PTV.Developer.Clients.routeoptimization.optiflow.Model
             this.Profile = profile;
             // use default value if no "speedFactor" provided
             this.SpeedFactor = speedFactor ?? 1D;
-            // use default value if no "allowViolations" provided
-            this.AllowViolations = allowViolations ?? true;
+            this.Violations = violations;
         }
 
         /// <summary>
@@ -73,13 +78,6 @@ namespace PTV.Developer.Clients.routeoptimization.optiflow.Model
         public double? SpeedFactor { get; set; }
 
         /// <summary>
-        /// When enabled, a vehicle is permitted to travel on segments prohibited by its profile.  These prohibited segments will only be used when no valid alternatives are available. 
-        /// </summary>
-        /// <value>When enabled, a vehicle is permitted to travel on segments prohibited by its profile.  These prohibited segments will only be used when no valid alternatives are available. </value>
-        [DataMember(Name = "allowViolations", EmitDefaultValue = true)]
-        public bool? AllowViolations { get; set; }
-
-        /// <summary>
         /// Returns the string presentation of the object
         /// </summary>
         /// <returns>String presentation of the object</returns>
@@ -89,7 +87,7 @@ namespace PTV.Developer.Clients.routeoptimization.optiflow.Model
             sb.Append("class RouteOptimizationVehicleRouting {\n");
             sb.Append("  Profile: ").Append(Profile).Append("\n");
             sb.Append("  SpeedFactor: ").Append(SpeedFactor).Append("\n");
-            sb.Append("  AllowViolations: ").Append(AllowViolations).Append("\n");
+            sb.Append("  Violations: ").Append(Violations).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
