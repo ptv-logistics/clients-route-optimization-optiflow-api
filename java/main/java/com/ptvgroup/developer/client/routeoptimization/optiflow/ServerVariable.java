220524083de33d011f50b2d64f53a5523705141b
MachineUserPTV <github@ptvgroup.com>
2026-04-28 09:17:37 +0000

    Automatic synchronisation from developer.myptv.com

--- java/main/java/com/ptvgroup/developer/client/routeoptimization/optiflow/ServerVariable.java
+++ java/main/java/com/ptvgroup/developer/client/routeoptimization/optiflow/ServerVariable.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 /**
  * Representing a Server Variable for server URL template substitution.
  */
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-21T12:44:22.808035682Z[Etc/UTC]", comments = "Generator version: 7.5.0")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-28T08:59:21.832272784Z[Etc/UTC]", comments = "Generator version: 7.5.0")
 public class ServerVariable {
     public String description;
     public String defaultValue;
